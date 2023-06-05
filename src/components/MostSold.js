a144ff54c4249145a3484c9980035fa95586b3a9
Laith <laith.hilal@appliedtechnology.se>
2023-06-05 18:13:56 +0300

    fixed mostsold

--- src/components/MostSold.js
+++ src/components/MostSold.js
@@ -3,13 +3,14 @@ import products from '../products';
 import './Products.css';
 
 const MostSold = () => {
-  const mostSoldProducts = products.filter(product => [1, 3, 7].includes(product.id));
+  const mostSoldProducts = products.filter(product => [1, 2, 7].includes(product.id));
 
   return (
     <div className="product-container">
       <h1>أكثر المنتجات مبيعًا</h1>
       {mostSoldProducts.map(product => (
         <div className="product-card" key={product.id}>
+        {product.imageUrl && <img className="product-image" src={product.imageUrl} alt={product.name} />}
           <h2 className="product-name">{product.name}</h2>
           <p>{product.description}</p>
           <p className="product-price">Price: {product.price}</p>
